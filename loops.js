32bc9c36f6fe30b6cb57a4e4063bad8521fb8beb
Sam Reeve <sam.reeve95@gmail.com>
2019-04-26 06:13:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,8 +14,8 @@ function forLoop(array)
 {
   for(x=0;x<25;x++)
   {
-    array.push(`"I am ${x} strange loop${x === 0 ? '' : 's'}."`);
-    console.log(`"I am ${x} strange loop${x === 0 ? '' : 's'}."`);
+    array.push(`"I am ${x} strange loop${x === 1 ? '' : 's'}."`);
+    console.log(`"I am ${x} strange loop${x === 1 ? '' : 's'}."`);
   }
 }
 var ar = ["Stuff"];
